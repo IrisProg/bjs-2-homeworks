58d3a4d9077d8caf618fa86190d0d8626fce541d
Irina <delo_iris@mail.ru>
2023-07-09 16:53:25 +0300

    Arr

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,21 @@
 function compareArrays(arr1, arr2) {
+    return arr1.length === arr2.length && arr1.every((i ,j) => i === arr2[j]);
   
 }
 
 function getUsersNamesInAgeRange(users, gender) {
+    function findeAve(acc, item, index, arr) {
+        let sum = acc + item;
+        if (index === arr.length - 1) {
+          sum = sum/arr.length;
+        }
+        return sum;
+      }
+      if (users.length <= 0) {
+        return 0;
+      } else {
+        result = people.filter(e => e.gender === gender).map(ages => ages.age).reduce(findeAve, 0);
+      }  
+      return result;
   
 }
